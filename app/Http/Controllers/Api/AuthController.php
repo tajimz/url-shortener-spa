6d5310ddef415f0abb22bea4accee190de524880
Tajim <com.tajim@gmail.com>
2026-05-29 21:24:05 +0600

    implement sanctum auth

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //
+    function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required', // to identify where the user logged in
+
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid credentials.'
+            ], 401);
+        }
+        $token = $user->createToken($request->device_name)->plainTextToken;
+        return response()->json(['token' => $token]);
+    }
+
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'device_name' => 'required|string'
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        $token = $user->createToken($validated['device_name'])->plainTextToken;
+
+        return response()->json([
+            'message' => 'Account created successfully',
+            'token' => $token,
+            'user' => $user
+        ], 201);
+    }
+}
