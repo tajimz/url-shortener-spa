bae0c3f888d1c153ba9b21e5b2cb472c3a69ad4d
Tajim <com.tajim@gmail.com>
2026-05-31 00:42:20 +0600

    refactor(settings): remove unnecessary UI buttons from layout

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -30,13 +30,13 @@ const { isCurrentOrParentUrl } = useCurrentUrl();
 
 <template>
     <div class="px-4 py-6">
-        <Heading
+        <!-- <Heading
             title="Settings"
             description="Manage your profile and account settings"
-        />
+        /> -->
 
         <div class="flex flex-col lg:flex-row lg:space-x-12">
-            <aside class="w-full max-w-xl lg:w-48">
+            <!-- <aside class="w-full max-w-xl lg:w-48">
                 <nav
                     class="flex flex-col space-y-1 space-x-0"
                     aria-label="Settings"
@@ -57,7 +57,7 @@ const { isCurrentOrParentUrl } = useCurrentUrl();
                         </Link>
                     </Button>
                 </nav>
-            </aside>
+            </aside> -->
 
             <Separator class="my-6 lg:hidden" />
 
